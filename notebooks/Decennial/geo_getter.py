1fe45d5672535c4de5158f413bdaaad6140ca9b8
daragoldberg <dgoldberg@planning.nyc.gov>
2023-09-11 09:25:36 -0400

    new_viz_new_ownership

---
+++ notebooks/Decennial/geo_getter.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+from geos import *
+
+
+base_url = 'https://tigerweb.geo.census.gov/tigerwebmain/Files/'
+acre = 0.000247105
+
+places_int = [int(x) for x in maricopa_places]
+
+
+#get area data for cities and consolidated city (indianapolis)
+def make_city(year):
+    indy = pd.read_html(f'{base_url}tab20/tigerweb_tab20_concity_{year}_in.html')
+    indy = indy[0]
+
+    states = ['az','tx','fl']
+    pl = pd.DataFrame()
+    for state in states:
+        df = pd.read_html(f'{base_url}tab20/tigerweb_tab20_incplace_{year}_{state}.html')
+        dff = df[0]
+        pl = pd.concat([pl,dff])
+        
+    places = pd.concat([indy,pl])
+    places = places[['GEOID','BASENAME','AREALAND','AREAWATER']].copy()
+    places['AREALAND_ACRE'] = places.AREALAND * acre
+    
+    for col in places.columns[1:]:
+        places.rename(columns={col:f'{col}{year[-2:]}'},inplace=True)
+        
+    return places
+
+#get maricopa county
+def make_maricopa(year):
+    df = pd.read_html(f'{base_url}tab20/tigerweb_tab20_county_{year}_az.html')
+    df = df[0]   
+    df = df[['GEOID','BASENAME','AREALAND','AREAWATER']].copy()
+    df = df[df.GEOID==4013]
+    df['AREALAND_ACRE'] = df.AREALAND * acre
+    for col in df.columns[1:]:
+        df.rename(columns={col:f'{col}{year[-2:]}'},inplace=True)
+    return df
+
+def make_az(year):
+    df = pd.read_html(f'{base_url}tab20/tigerweb_tab20_state_{year}_us.html')
+    df = df[0]   
+    df = df[['GEOID','BASENAME','AREALAND','AREAWATER']].copy()
+    df = df[df.GEOID==4]
+    df['AREALAND_ACRE'] = df.AREALAND * acre
+    for col in df.columns[1:]:
+        df.rename(columns={col:f'{col}{year[-2:]}'},inplace=True)
+    return df
+
+def make_maricopa_pl(year):
+    df = pd.read_html(f'{base_url}tab20/tigerweb_tab20_incplace_{year}_az.html')
+    dff = pd.read_html(f'{base_url}tab20/tigerweb_tab20_cdp_{year}_az.html')
+    df,dff = df[0],dff[0]
+    df = pd.concat([df,dff])
+    df = df[df.GEOID.isin(places_int)]
+    df = df[['GEOID','BASENAME','AREALAND','AREAWATER']].copy()
+    df['AREALAND_ACRE'] = df.AREALAND * acre
+    for col in df.columns[1:]:
+        df.rename(columns={col:f'{col}{year[-2:]}'},inplace=True)
+    return df
+
+#get us
+def make_us(year):
+    df = pd.read_html(f'{base_url}tab20/tigerweb_tab20_region_{year}_us.html')
+    df = df[0]   
+    df = df[['FUNCSTAT','AREALAND','AREAWATER']].copy().groupby(['FUNCSTAT']).sum().reset_index()
+    df['AREALAND_ACRE'] = df.AREALAND * acre
+    for col in df.columns[1:]:
+        df.rename(columns={col:f'{col}{year[-2:]}'},inplace=True)
+    return df
---
+++ notebooks/Decennial/geos.py
@@ -0,0 +1,38 @@
+## LIST OF GEOGRAPHIES
+phx_pumas = ['0400112','0400113','0400114','0400115','0400116','0400117',\
+             '0400118','0400119','0400120','0400121','0400122','0400123',\
+             '0400125','0400128','0400129']
+
+maricopa_places=['0455000','0454050','0458150','0410180','0411300','0427050','0427400',\
+                 '0428380','0452930','0465000','0482740','0404720','0407940','0412000',\
+                 '0422220','0425300','0427820','0430270','0441330','0471510','0474190',\
+                 '0485400','0446000','0473000','0400730','0402430','0404020','0413820',\
+                 '0413610','0436780','0447750','0473490','0427260','0428465','0438655',\
+                 '0444450','0449360','0460250','0462362','0470320','0470355','0470530',\
+                 '0474540','0484000','0484140']
+
+
+maricopa_places_dict={'0455000':'Phoenix','0454050':'Peoria','0458150':'Queen Creek',\
+                 '0410180':'Carefree','0411300':'Cave Creek','0427050':'Gila Bend','0427400':'Gilbert',\
+                 '0428380':'Goodyear','0452930':'Paradise Valley','0465000':'Scottsdale',\
+                 '0482740':'Wickenburg','0404720':'Avondale','0407940':'Buckeye',\
+                 '0412000':'Chandler','0422220':'El Mirage','0425300':'Fountain Hills',\
+                 '0427820':'Glendale','0430270':'Guadalupe','0441330':'Litchfield Park',\
+                 '0471510':'Surprise','0474190':'Tolleson','0485400':'Youngtown',\
+                 '0446000':'Mesa','0473000':'Tempe','0400730':'Aguila','0402430':'Anthem',\
+                 '0404020':'Arlington','0413820':'Citrus Park','0413610':'Circle City',\
+                 '0436780':'Kaka','0447750':'Morristown','0473490':'Theba',\
+                 '0427260':'Gila Crossing','0428465':'Goodyear Village','0438655':'Komatke',\
+                 '0444450':'Maricopa Colony','0449360':'New River','0460250':'Rio Verde',\
+                 '0462362':'St. Johns','0470320':'Sun City','0470355':'Sun City West',\
+                 '0470530':'Sun Lakes','0474540':'Tonopah','0484000':'Wintersburg',\
+                 '0484140':'Wittmann'}
+
+comp_cities = {'48':['65000','35000'],'12':['35000'],'04':['77000'],\
+              '35':['02000']} 
+
+#AZ:tempe,scottsdale,glendale TX: san antonio, houston
+#FL: jacksonville
+#Indianapolis consolidated city: '18','36000' - has to be imported separately
+
+#removed: louisville, ky '21':['48000'] - no data in 2010, removed dallas 4819000
